bf3649d27ac5cb8ccbe284a4785027252e498f20
js324 <jins918@gmail.com>
2023-11-16 17:08:25 -0500

    fraud

--- app.py
+++ app.py
@@ -309,8 +309,6 @@ def fraud():
                 if (pair[0] == pair[1]):
                     continue
                 if (len(sendersDict[pair[1]]) != 0):
-                    if (pair[0] not in sendersDict[pair[1]]):
-                        if (dfs(pair[0], pair[1], set(), recDict)):
                             ans = "Ineligible"
                 sendersDict[pair[0]].add(int(pair[1]))
                 recDict[pair[1]].add(pair[0])
